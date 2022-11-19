ba5da21381c94bec14a78550957b4928e2ad7e2f
Junho Yoon(a.k.a Hoplin) <45956041+J-hoplin1@users.noreply.github.com>
2022-11-19 13:55:51 +0900

    Revert "feat : Porting plain html to PHP"

--- home/common/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once $_SERVER['DOCUMENT_ROOT']."/home/common/constant.php";
-include_once TEMPLATES_PATH."/header.php";
-include_once TEMPLATES_PATH."/footer.php";
-include_once TEMPLATES_PATH."/main-titles/index.php";
-?>
--- home/login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once $_SERVER['DOCUMENT_ROOT']."/home/common/constant.php";
-session_start();
-// Destroy session id and session variables
-session_destroy();
-
-header("Location: ".HOME_PATH);
-?>
